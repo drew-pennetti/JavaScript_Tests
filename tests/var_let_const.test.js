b9d0dd366691b12474b23ef670612a412922fbe0
Drew Pennetti <drew.pennetti@hscloudsuite.com>
2021-11-03 12:26:24 -0400

    updated var let const

--- tests/var_let_const.test.js
+++ tests/var_let_const.test.js
@@ -1,17 +1,17 @@
 test("fix below so that x is equal to 1 ", () => {
   var x = 1;
   var x = 2;
-  expect(x).toB(1);
+  expect(x).toBe(1);
 });
 
 test("fix below so so no error is thrown", () => {
   let x = 1;
   let x = 1;
-  expect(x).toB(1);
+  expect(x).toBe(1);
 });
 
 test("fix below so so no error is thrown", () => {
   const x = 1;
   const x = 1;
-  expect(x).toB(1);
+  expect(x).toBe(1);
 });
